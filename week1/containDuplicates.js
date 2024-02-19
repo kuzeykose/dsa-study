235576119326ba37ed2c34ae3a988513e0fdfc7c
caanyvzdb <mcanyvz1996@gmail.com>
2024-02-19 18:40:12 +0300

    Update containDuplicates.js

--- week1/containDuplicates.js
+++ week1/containDuplicates.js
@@ -5,6 +5,9 @@
 var containsDuplicate = function (nums) {
   let duplicateArr = [];
   for (let x = 0; x < nums.length; x++) {
+    if (nums[x] === 0) {
+      continue;
+    }
     for (let y = 0; y < nums.length; y++) {
       if (x === y) {
         continue;

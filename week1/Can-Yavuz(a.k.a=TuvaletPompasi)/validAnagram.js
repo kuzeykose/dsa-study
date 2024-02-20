9e0e2557bd21477d7d0d324b31e9a351e8d04d25
caanyvzdb <mcanyvz1996@gmail.com>
2024-02-20 13:12:28 +0300

    Update validAnagram.js

--- week1/Can-Yavuz(a.k.a=TuvaletPompasi)/validAnagram.js
+++ week1/Can-Yavuz(a.k.a=TuvaletPompasi)/validAnagram.js
@@ -5,5 +5,18 @@
  */
 var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
-  return s.split('').sort().join('') === t.split('').sort().join('');
+
+  const charCountS = new Array(26).fill(0);
+  const charCountT = new Array(26).fill(0);
+
+  for (let i = 0; i < s.length; i++) {
+    charCountS[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    charCountT[t.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+  }
+
+  for (let i = 0; i < 26; i++) {
+    if (charCountS[i] !== charCountT[i]) return false;
+  }
+
+  return true;
 };

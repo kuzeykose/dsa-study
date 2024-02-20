6fdca512d5678e813b69ba76724ddb5bd3fa1614
caanyvzdb <mcanyvz1996@gmail.com>
2024-02-20 13:09:30 +0300

    Update validAnagram.ts

--- week1/Can-Yavuz(a.k.a=TuvaletPompasi)/validAnagram.ts
+++ week1/Can-Yavuz(a.k.a=TuvaletPompasi)/validAnagram.ts
@@ -1,6 +1,16 @@
 function isAnagram(s: string, t: string): boolean {
     if (s.length !== t.length) return false;
-   const sortedS: string = s.split('').sort().join('');
-   const sortedT: string = t.split('').sort().join('');
-   return sortedS === sortedT;
-};
+
+    const charCount: Record<string, number> = {};
+
+    for (let char of s) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (let char of t) {
+        if (!charCount[char]) return false;
+        charCount[char]--;
+    }
+
+    return true;
+}

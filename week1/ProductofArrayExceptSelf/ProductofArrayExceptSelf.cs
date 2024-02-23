a422a6d06456d8fadcaa69e505cb05a528eab99e
busra2 <busra.kemter@outlook.com>
2024-02-24 01:27:04 +0300

    6/14 questions solution

---
+++ week1/ProductofArrayExceptSelf/ProductofArrayExceptSelf.cs
@@ -0,0 +1,35 @@
+﻿public class Solution {
+    public static int[] ProductExceptSelf(int[] nums) {
+        int n = nums.Length;
+
+       
+        int[] leftProducts = new int[n];
+        leftProducts[0] = 1;
+        for (int i = 1; i < n; i++) {
+            leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+        }
+
+        
+        int rightProduct = 1;
+
+      
+        int[] result = new int[n];
+
+        
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = leftProducts[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+
+    public static void Main() {
+        // Örnek kullanım
+        int[] nums1 = {1, 2, 3, 4};
+        Console.WriteLine($"Example 1: [{string.Join(", ", ProductExceptSelf(nums1))}]");
+
+        int[] nums2 = {-1, 1, 0, -3, 3};
+        Console.WriteLine($"Example 2: [{string.Join(", ", ProductExceptSelf(nums2))}]");
+    }
+}
---
+++ week1/TwoSum/TwoSum.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+public class Solution {
+    public static int[] TwoSum(int[] nums, int target) {
+        // Sayıları ve index'lerini tutan bir sözlük oluşturuyoruz.
+        Dictionary<int, int> numIndices = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+
+            // Eğer complement değeri sözlükte varsa, bu iki sayının toplamı hedefe eşittir.
+            if (numIndices.ContainsKey(complement)) {
+                return new int[] { numIndices[complement], i };
+            }
+
+            // Sözlüğe güncel sayıyı ve index'ini ekliyoruz.
+            if (!numIndices.ContainsKey(nums[i])) {
+                numIndices[nums[i]] = i;
+            }
+        }
+
+        // Eğer çözüm bulunamazsa null döndürüyoruz.
+        return null;
+    }
+
+    public static void Main() {
+        // Örnek kullanımlar
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        Console.WriteLine($"Example 1: [{string.Join(", ", TwoSum(nums1, target1))}]"); // [0, 1]
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        Console.WriteLine($"Example 2: [{string.Join(", ", TwoSum(nums2, target2))}]"); // [1, 2]
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        Console.WriteLine($"Example 3: [{string.Join(", ", TwoSum(nums3, target3))}]"); // [0, 1]
+    }
+}

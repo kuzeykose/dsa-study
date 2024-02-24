3bb03b54ac6d4e3e9dda87835fa7500f8864bd7e
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2024-02-24 21:28:20 +0300

    Omer Solutions for week-1 (#2)
    * feat(week1): add contains duplicate javascript solution
    
    * feat(week1): add valid anagram javascript solution
    
    * feat(week1): add two sum javascript solution
    
    * fix(week1): update file name
    
    * feat(week1): add group anagrams javascript solution
    
    * feat(week1): add all javascript solution without encode/decode

---
+++ week1/omerSefaYilmaz/containsDuplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var containsDuplicate = function(nums) {
+    const set = new Set(nums)
+    return nums.length == set.size ? false : true
+};

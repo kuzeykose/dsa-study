4e3800de4ee05d8086ba065f11b143c292e3cabe
Adrian Yigit <bilalyigit594@gmail.com>
2024-02-27 17:32:58 +0300

    Week 1 solutions (#17)

---
+++ week1/MarshalLee/IsPalindrome.cs
@@ -0,0 +1,24 @@
+﻿public bool IsPalindrome(string s)
+{
+    string lowerString = s.ToLower();
+    int left = 0, right = lowerString.Length - 1;
+    while (left < right)
+    {
+        if (!char.IsLetterOrDigit(lowerString[left]))
+        {
+            left++;
+        }
+        else if (!char.IsLetterOrDigit(lowerString[right]))
+        {
+            right--;
+        }
+        else
+        {
+            if (lowerString[left] != lowerString[right])
+                return false;
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
---
+++ week1/MarshalLee/IsValidSudoku.cs
@@ -0,0 +1,21 @@
+﻿public bool IsValidSudoku(char[][] board)
+{
+    HashSet<(int, int)> rows = new();
+    HashSet<(int, int)> columns = new();
+    HashSet<(int, int, int)> subBoxes = new();
+
+    for (int ri = 0; ri < 9; ri++)
+    {
+        for (int ci = 0; ci < 9; ci++)
+        {
+            if (board[ri][ci] == '.')
+                continue;
+
+            if (!rows.Add((ri, board[ri][ci])) ||
+                !columns.Add((ci, board[ri][ci])) ||
+                !subBoxes.Add((ri / 3, ci / 3, board[ri][ci])))
+                return false;
+        }
+    }
+    return true;
+}

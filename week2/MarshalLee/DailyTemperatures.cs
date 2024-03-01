ceaf5a333874ee7cc8551221fda38f42b5a07814
Adrian Yigit <bilalyigit594@gmail.com>
2024-03-01 18:04:40 +0300

    Week 2 solutions

---
+++ week2/MarshalLee/DailyTemperatures.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Class1
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        Stack<int> stack = new();
+        int[] array = new int[temperatures.Length];
+        for (int i = temperatures.Length - 1; i >= 0; i--)
+        {
+            while (stack.Count > 0 && temperatures[stack.Peek()] <= temperatures[i])
+                stack.Pop();
+            if (stack.Count > 0)
+                array[i] = stack.Peek() - i;
+            stack.Push(i);
+        }
+        return array;
+        //Dictionary<int, int> dict = new();
+        //int j = 0;
+        //for (int i = 0; i <= temperatures.Length - 1; i++)
+        //{
+        //    dict.Add(temperatures[i], 0);
+        //    while (j < i)
+        //    {
+        //        if (dict.ElementAt(j).Key < temperatures[i])
+        //        {
+        //            dict[temperatures[j]] = i - j;
+        //            j++;
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+        //    }
+        //}
+
+        //return dict.Values.ToArray();
+    }
+}
---
+++ week2/MarshalLee/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Stack<T>
+{
+    public int Length;
+    private Node<T>? Head;
+
+    public Stack()
+    {
+        this.Head = null;
+        this.Length = 0;
+    }
+
+    public void Push(T data)
+    {
+        Node<T> node = new(data);
+        this.Length++;
+
+        if (this.Head is null)
+        {
+            this.Head = node;
+            return;
+        }
+
+        node.Prev = this.Head;
+        this.Head = node;
+    }
+
+    public T? Pop()
+    {
+        this.Length = Math.Max(0, this.Length - 1);
+
+        if (this.Length == 0)
+        {
+            Node<T>? head0 = this.Head as Node<T>;
+            this.Head = null;
+            return head0.Data;
+        }
+
+        Node<T>? head = this.Head as Node<T>;
+        this.Head = head.Prev;
+
+        return head.Data;
+    }
+
+    public T? Peek<T>() where T : class
+    {
+        return this.Head is not null ? this.Head.Data as T : null;
+    }
+}

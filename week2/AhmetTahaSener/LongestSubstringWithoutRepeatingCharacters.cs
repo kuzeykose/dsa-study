dced327f33273ed41a07de67e5358ffa49d4c99c
ahmettahasener <110438048+ahmettahasener@users.noreply.github.com>
2024-03-07 07:49:31 +0300

    Added Week2 (#34)
    * Added Week1
    
    * AddedWeek2

---
+++ week2/AhmetTahaSener/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> substr = new HashSet<char>();
+        int l = 0, r = 0; // left, right index
+        int longestSubstring = 0;
+
+        while (r < s.Length)
+        {
+            if (!substr.Contains(s[r]))
+            {
+                substr.Add(s[r++]);
+                longestSubstring = Math.Max(longestSubstring, r - l);
+            }
+            else
+            {
+                substr.Remove(s[l++]);
+            }
+        }
+
+        return longestSubstring;
+    }
+}

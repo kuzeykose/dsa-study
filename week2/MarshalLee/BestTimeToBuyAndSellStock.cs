40c21e18e398d781e34f0675caa12a1f9be38462
Adrian Yigit <bilalyigit594@gmail.com>
2024-03-07 07:46:38 +0300

    Week 2 solutions (#26)
    * Week 1 solutions
    
    * Week 2 solutions

---
+++ week2/MarshalLee/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class BestTimeToBuyAndSellStock
+{
+    public int MaxProfit(int[] prices)
+    {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            minPrice = Math.Min(minPrice, prices[i]);
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+}

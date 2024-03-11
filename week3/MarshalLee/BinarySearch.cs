914eb571031c6c66b08530436ba4f49165df1c90
Adrian Yigit <bilalyigit594@gmail.com>
2024-03-11 18:24:28 +0300

    First 3 algorithms

---
+++ week3/MarshalLee/BinarySearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public int Search(int[] nums, int target)
+{
+    int left = 0, right = nums.Length - 1;
+    while (left <= right)
+    {
+        int index = (left + right) / 2;
+        if (target > nums[index])
+            left = index + 1;
+        else if (target < nums[index])
+            right = index - 1;
+        else
+            return index;
+    }
+    return -1;
+
+}
---
+++ week3/MarshalLee/SearchMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public bool SearchMatrix(int[][] matrix, int target)
+{
+    int index = 0;
+
+    if (matrix.Length == 1 && matrix[0].Length == 1)
+    {
+        return matrix[0][0] == target;
+    }
+
+    while (index < matrix.Length)
+    {
+        if (!(matrix[index][0] <= target && target <= matrix[index][^1]))
+        {
+            index++;
+            continue;
+        }
+        else
+        {
+            int left = 0, right = matrix[index].Length - 1;
+            while (left <= right)
+            {
+                int i = (left + right) / 2;
+                if (target > matrix[index][i])
+                    left = i + 1;
+                else if (target < matrix[index][i])
+                    right = i - 1;
+                else
+                    return true;
+            }
+            return false;
+        }
+    }
+    return false;
+}

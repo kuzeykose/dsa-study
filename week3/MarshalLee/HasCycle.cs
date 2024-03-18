da91d65c0141be2098a6312c5ad6ff86106bc864
Adrian Yigit <bilalyigit594@gmail.com>
2024-03-18 18:38:21 +0300

    LinkedList cycle algorithm

---
+++ week3/MarshalLee/HasCycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public bool HasCycle(ListNode head)
+{
+    HashSet<ListNode> visited = new();
+    ListNode currentNode = head;
+    while (currentNode != null)
+    {
+        if (visited.Contains(currentNode))
+        {
+            return true;
+        }
+        visited.Add(currentNode);
+        currentNode = currentNode.next;
+    }
+    return false;
+    //if (head == null) return false;
+
+    //ListNode fast = head;
+    //ListNode slow = head;
+
+    //while (fast.next != null && fast.next.next != null)
+    //{
+    //    fast = fast.next.next;
+    //    slow = slow.next;
+
+    //    if (fast == slow) return true;
+    //}
+    //return false;
+}

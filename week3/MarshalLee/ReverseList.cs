223360ee6a9038d8dbffd204dbf214cb61632259
Adrian Yigit <bilalyigit594@gmail.com>
2024-03-19 16:30:52 +0300

    Reverse Linked List

---
+++ week3/MarshalLee/ReverseList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static ListNode ReverseList(ListNode head)
+{
+    ListNode previousNode = null;
+    while (head != null)
+    {
+        ListNode currNext = head.next;
+        head.next = previousNode;
+        previousNode = head;
+        head = currNext;
+    }
+    return previousNode;
+    //ListNode previous = null;
+    //ListNode curr = head;
+    //while(curr != null)
+    //{
+    //    ListNode next = curr.next;
+    //    curr.next = previous;
+    //    previous = curr;
+    //    curr = next;
+    //}
+    //return previous;
+}
